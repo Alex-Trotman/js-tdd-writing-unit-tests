4fea55d824d3bed7986704f9075ed21f60150d1d
Alex Trotman <alextrotman04@gmail.com>
2023-11-16 10:54:17 -0600

    finish

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -9,4 +9,13 @@ describe("pointsForWord", () => {
 
     expect(points).toBe(7);
   });
+
+  it("handles uppercase and lowercase input", () => {
+    const word = "tEsT";
+  
+    const points = pointsForWord(word);
+  
+    expect(points).toBe(7);
+  });
+
 });
